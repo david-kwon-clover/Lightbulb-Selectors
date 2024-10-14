b62bc7c1a2f8f3d0053184215b6ccd7773d60b3f
David Kwon <david.kwon@clover.com>
2024-10-14 10:52:00 -0600

    Implement click event listeners for each light bulb to toggle active class and increment count

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const lightBulb3 = document.querySelector("#lightbulb3");
 const clickCountSubtitle = document.querySelector(".subtitle");
 
 let count = 0; 
+
+lightBulb1.addEventListener("click", function() {
+    count++;
+    clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`;
+    this.classList.toggle("active");
+})
+lightBulb2.addEventListener("click", function() {
+    count++;
+    clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`;
+    this.classList.toggle("active");
+})
+lightBulb3.addEventListener("click", function() {
+    count++;
+    clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`;
+    this.classList.toggle("active");
+})
