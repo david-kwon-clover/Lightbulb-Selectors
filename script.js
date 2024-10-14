186e882d6d102e2c357e0bc8bfb9fb6d11be5158
David Kwon <david.kwon@clover.com>
2024-10-14 10:47:28 -0600

    Use querySelector to select each light bulb

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Write your code here
 console.log("Script connected");
+
+const lightBulb1 = document.querySelector("#lightbulb1");
+const lightBulb2 = document.querySelector("#lightbulb2");
+const lightBulb3 = document.querySelector("#lightbulb3");
+
