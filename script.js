61d6fd10ff5e287d93ab49c92c780d00565cf251
David Kwon <david.kwon@clover.com>
2024-10-14 11:01:40 -0600

    Refactor to select all light bulbs using querySelectorAll

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
 // Write your code here
 console.log("Script connected");
 
-const lightBulb1 = document.querySelector("#lightbulb1");
-const lightBulb2 = document.querySelector("#lightbulb2");
-const lightBulb3 = document.querySelector("#lightbulb3");
+// const lightBulb1 = document.querySelector("#lightbulb1");
+// const lightBulb2 = document.querySelector("#lightbulb2");
+// const lightBulb3 = document.querySelector("#lightbulb3");
+
+const lightBulbs = document.querySelectorAll(".lightbulb");
 
 const clickCountSubtitle = document.querySelector(".subtitle");
 
 let count = 0;
 
-lightBulb1.addEventListener("click", function () {
-  count++;
-  count === 1
-    ? (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} time`)
-    : (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`);
-  this.classList.toggle("active");
-});
-lightBulb2.addEventListener("click", function () {
-  count++;
-  count === 1
-    ? (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} time`)
-    : (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`);
-  this.classList.toggle("active");
-});
-lightBulb3.addEventListener("click", function () {
-  count++;
-  count === 1
-    ? (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} time`)
-    : (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`);
-  this.classList.toggle("active");
-});
+// lightBulb1.addEventListener("click", function () {
+//   count++;
+//   count === 1
+//     ? (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} time`)
+//     : (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`);
+//   this.classList.toggle("active");
+// });
+// lightBulb2.addEventListener("click", function () {
+//   count++;
+//   count === 1
+//     ? (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} time`)
+//     : (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`);
+//   this.classList.toggle("active");
+// });
+// lightBulb3.addEventListener("click", function () {
+//   count++;
+//   count === 1
+//     ? (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} time`)
+//     : (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`);
+//   this.classList.toggle("active");
+// });
+
+lightBulbs.forEach((lightBulb) =>
+  lightBulb.addEventListener("click", function () {
+    count++;
+    count === 1
+      ? (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} time`)
+      : (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`);
+    this.classList.toggle("active");
+  })
+);
