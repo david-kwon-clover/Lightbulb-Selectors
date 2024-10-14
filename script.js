02d1d52759b6f9bb287f54914cde7b05eb830082
David Kwon <david.kwon@clover.com>
2024-10-14 10:55:54 -0600

    Add conditional logic for grammatically correct text when count is 1

--- script.js
+++ script.js
@@ -7,20 +7,26 @@ const lightBulb3 = document.querySelector("#lightbulb3");
 
 const clickCountSubtitle = document.querySelector(".subtitle");
 
-let count = 0; 
+let count = 0;
 
-lightBulb1.addEventListener("click", function() {
-    count++;
-    clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`;
-    this.classList.toggle("active");
-})
-lightBulb2.addEventListener("click", function() {
-    count++;
-    clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`;
-    this.classList.toggle("active");
-})
-lightBulb3.addEventListener("click", function() {
-    count++;
-    clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`;
-    this.classList.toggle("active");
-})
+lightBulb1.addEventListener("click", function () {
+  count++;
+  count === 1
+    ? (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} time`)
+    : (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`);
+  this.classList.toggle("active");
+});
+lightBulb2.addEventListener("click", function () {
+  count++;
+  count === 1
+    ? (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} time`)
+    : (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`);
+  this.classList.toggle("active");
+});
+lightBulb3.addEventListener("click", function () {
+  count++;
+  count === 1
+    ? (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} time`)
+    : (clickCountSubtitle.innerHTML = `You've clicked the lights ${count} times`);
+  this.classList.toggle("active");
+});
