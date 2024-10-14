ffe238c51e1c5e8f34377470103c50c4370c8c69
David Kwon <david.kwon@clover.com>
2024-10-14 10:48:46 -0600

    Use querySelector to select the click counter subtitle

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ const lightBulb1 = document.querySelector("#lightbulb1");
 const lightBulb2 = document.querySelector("#lightbulb2");
 const lightBulb3 = document.querySelector("#lightbulb3");
 
+const clickCountSubtitle = document.querySelector(".subtitle");
